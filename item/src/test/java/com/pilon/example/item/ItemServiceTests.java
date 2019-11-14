e7d4109e91081599569463c4ce4c51cb384af6cc
Chuck Pilon <chuck.pilon@gmail.com>
2019-11-14 09:30:16 -0600

    Rename DemoApplicationTests to ItemServiceTests.

--- item/src/test/java/com/pilon/example/item/ItemServiceTests.java
+++ item/src/test/java/com/pilon/example/item/ItemServiceTests.java
@@ -1,6 +1,5 @@
 package com.pilon.example.item;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ItemServiceTests {
 
-	@Ignore
 	@Test
 	public void contextLoads() {
 	}
