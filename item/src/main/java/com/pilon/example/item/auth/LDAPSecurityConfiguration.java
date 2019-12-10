a6794e28575b54a2dbf3da27c5279430ec0096d5
Chuck Pilon <chuck.pilon@gmail.com>
2019-12-10 16:30:33 -0600

    Read LDAP authorities.

--- item/src/main/java/com/pilon/example/item/auth/LDAPSecurityConfiguration.java
+++ item/src/main/java/com/pilon/example/item/auth/LDAPSecurityConfiguration.java
@@ -1,13 +1,16 @@
 package com.pilon.example.item.auth;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 
 @Configuration
 @SuppressWarnings("deprecation")
@@ -26,13 +29,6 @@ public class LDAPSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${ldap.passwordAttribute}")
     String passwordAttribute;
 
-    // @Override
-    // protected void configure(HttpSecurity http) throws Exception {
-    //     http.httpBasic();
-
-    //     http.authorizeRequests().anyRequest().authenticated();
-    // }
-
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -49,18 +45,26 @@ public class LDAPSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http
 			.logout().logoutUrl("/logout").logoutSuccessUrl("/logout-success").permitAll();
 
-	}
+    }
 
+    @Bean
+    public LdapContextSource contextSource() {
+        LdapContextSource contextSource = new LdapContextSource();
+        contextSource.setUrl(contextURL);
+        return contextSource;
+    }    
+    
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        
+
+        LdapAuthoritiesPopulator populator = new DefaultLdapAuthoritiesPopulator(contextSource(), groupSearchBase);
+
         // @formatter:off
         auth.ldapAuthentication()
             .userDnPatterns(userDnPatterns)
             .groupSearchBase(groupSearchBase)
-            .contextSource()
-                .url(contextURL)
-            .and()
+            .ldapAuthoritiesPopulator(populator)
+            .contextSource(contextSource())
             .passwordCompare()
                 .passwordAttribute(passwordAttribute)
                 .passwordEncoder(new LdapShaPasswordEncoder());
